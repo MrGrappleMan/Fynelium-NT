91bc5c8849f55cefc3c81cf132ff0b454b2a4d45
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2025-08-09 17:06:51 +0530

    Update main.bat

--- main.bat
+++ main.bat
@@ -12,11 +12,102 @@ pause>nul
 exit
 )
 set seperator=echo _____________________________________________________________________________________________________________________________________________________________________________________________
-set svcopt="if !el!==1 (sc stop "!svcnme!" ^& sc config "!svcnme!" start=disabled) ^& if !el!==2 (sc start "!svcnme!" ^& sc config "!svcnme!" start=auto)"
-set toggleshow="%seperator% ^& echo Options: ^& echo X. Return to main menu ^& echo 1. Disable ^& echo 2. Enable ^& choice /C 12X /N"
+set svcset="if !el!==1 (sc stop "!svcnme!" ^& sc config "!svcnme!" start=disabled) ^& if !el!==2 (sc start "!svcnme!" ^& sc config "!svcnme!" start=auto)"
+set userask="echo Options: ^& echo X. Skip ^& echo 1. Disable ^& echo 2. Enable ^& choice /C 12X /N"
 color 07
-powercfg -h on
+
+cls
+echo Hello %username%! I am not responsible for any data loss, malfunctioning or any kind of damage done to your device.
+echo YOU have chosen to do this modification. Save all your work before proceeding. Leaving your device idle when the process starts is recommended.
+%seperator%
+echo PRESS ANY KEY TO START
+pause>nul
+cls
+
+	echo Name: Printing or Fax
+    echo TLDR: Disable if you do not use a printer, fax or a virtual print service.
+	echo.
+	echo Manages print jobs sent from the computer to the printer or print server
+	echo It can store multiple print jobs in the print queue or buffer retrieved by the printer or print server
+	!userask!
+	set svcnme=Spooler
+	!svcset!
+	set svcnme=Fax
+	!svcset!
+	cls
+
+	echo Name: Windows Image Acquisition
+	echo Waits until you press the button on your scanner and then manages the process of getting the image where it needs to go
+	echo This also affects communication with cameras and Android PTP that you connect directly to your computer, so be aware of that if you need this function
+	!userask!
+	set svcnme=StiSvc
+	!svcset!
+	cls
+
+	echo Name: Blutooth
+    echo TLDR: Disable if you do not use Bluetooth
+	echo.
+	echo Stopping this service causes paired Bluetooth devices to fail to operate
+	echo It prevent new devices from being discovered or paired
+	echo Yet it can also serve as a safety measure from attacks like KNOB or BLUFFS
+	!userask!
+	set svcnme=BTAGService
+	!svcset!
+	set svcnme=bthserv
+	!svcset!
+	cls
+
+	echo Name: Remote Access
+	echo TLDR: Disable if you don't use remote desktop or remotely manage your device
+	echo.
+	echo These services make remote control of your computer possible.
+	echo However, Microsoft Support could use this to fix issues.
+	echo Windows's Remote support won't work if you disable these services.
+	echo So, disabling these helps improve the security of your device, as they can be used in fake support scams.
+	echo You may use Chrome Remote Desktop or most other 3rd party apps without any issues.
+	!userask!
+	set svcnme=SessionEnv
+	!svcset!
+	set svcnme=TermService
+	!svcset!
+	set svcnme=UmRdpService
+	!svcset!
+    set svcnme=RemoteRegistry
+	!svcset!
+	cls
+
+	echo Name: Virtualization or Containerization
+	echo TLDR: Enable if you use Docker, VirtualBox, Hyper-V, WSL, VMware, Bluestacks, or any other virtualization or emulation software
+	echo.
+	!userask!
+	if !el!==1 (bcdedit /set hypervisorlaunchtype off)
+	if !el!==2 (bcdedit /set hypervisorlaunchtype on)
+    cls
+
+::Trigger Tweaks OR Repairs
+sfc /scannow
+chkdsk /f /r
+wsreset.exe
+dism /Online /CheckHealth 
+dism /Online /ScanHealth
+dism /Online /Cleanup-Image /RestoreHealth
+net stop wuauserv>nul
+del /F /S /Q %windir%\SoftwareDistribution\Download\*>nul
+del /F /S /Q %tmp%\*>nul
+del /F /S /Q %windir%\Temp\*>nul
+del /F /S /Q %windir%\Prefetch\*>nul
+ipconfig /flushdns>nul
+ipconfig /registerdns>nul
+ipconfig /release>nul
+ipconfig /renew>nul
+wuauclt.exe /updatenow>nul
+net start wuauserv>nul
+
+:: Toggle Tweaks:
+powercfg -h off
+
 sc start "UsoSvc">nul & sc config "UsoSvc" start=auto>nul
+sc start "InstallServicec">nul & sc config "InstallService" start=auto>nul
 sc start "DiagTrack">nul & sc config "DiagTrack" start=auto>nul
 sc start "tzautoupdate">nul & sc config "tzautoupdate" start=auto>nul
 sc start "BITS">nul & sc config "BITS" start=auto>nul
@@ -34,11 +125,10 @@ sc start "dot3svc">nul & sc config "dot3svc" start=auto>nul
 sc start "SysMain">nul & sc config "SysMain" start=auto>nul
 sc stop "WSearch">nul & sc config "WSearch" start=disabled>nul
 
-:: For all:
 bcdedit /set bootlog yes
 bcdedit /set bootmenupolicy Standard
 bcdedit /set bootstatuspolicy DisplayAllFailures
-bcdedit /set quietboot off
+bcdedit /set quietboot on
 bcdedit /set sos on
 bcdedit /set nocrashautoreboot off
 bcdedit /set bootuxdisabled off
@@ -52,8 +142,8 @@ bcdedit /set maxgroup on
 bcdedit /set onecpu off
 bcdedit /set vsmlaunchtype Auto
 bcdedit /set nx Optin
+bcdedit /deletevalue useplatformclock
 
-:: Unstable or compatibility issues:
 :: bcdedit /set forcelegacyplatform no
 :: bcdedit /set nolowmem off
 :: bcdedit /set x2apicpolicy enable
@@ -65,120 +155,16 @@ bcdedit /set nx Optin
 :: bcdedit /set testsigning No
 :: bcdedit /set hypervisorlaunchtype on
 
-powercfg.exe -import "!cd!\powerplan.pow">nul
 w32tm /config /syncfromflags:manual /manualpeerlist:"time.google.com time.windows.com time.cloudflare.com pool.ntp.org time.facebook.com time.apple.com time.aws.com" /reliable:YES /update & net stop w32time & net start w32time & w32tm /resync /force
+
 regedit /s registry.reg
-:home
-cls
-echo  ________   ___    ___  ________       _______       ___           ___      ___  ___      _____ ______      
-echo |\  _____\ |\  \  /  /||\   ___  \    |\  ___ \     |\  \         |\  \    |\  \|\  \    |\   _ \  _   \    
-echo \ \  \__/  \ \  \/  / /\ \  \\ \  \   \ \   __/|    \ \  \        \ \  \   \ \  \\\  \   \ \  \\\__\ \  \   
-echo  \ \   __\  \ \    / /  \ \  \\ \  \   \ \  \_|/__   \ \  \        \ \  \   \ \  \\\  \   \ \  \\|__| \  \  
-echo   \ \  \_|   \/  /  /    \ \  \\ \  \   \ \  \_|\ \   \ \  \____    \ \  \   \ \  \\\  \   \ \  \    \ \  \ 
-echo    \ \__\  __/  / /       \ \__\\ \__\   \ \_______\   \ \_______\   \ \__\   \ \_______\   \ \__\    \ \__\
-echo     \|__| |\___/ /         \|__| \|__|    \|_______|    \|_______|    \|__|    \|_______|    \|__|     \|__|
-echo           \|___|/
-%seperator%
-echo Hello %username%! I am not responsible for any data loss, malfunctioning or any kind of damage done to your device.
-echo YOU have chosen to do this modification. Save your work before proceeding. After tweaking, please reboot manually.
-%seperator%
-echo Options:
-echo X. Exit
-echo 1. Start
-choice /C 1X /N
-cls
 
-if !el!==1 (
-	echo Name: Printing
-        echo Disable if you do not use a printer or a virtual print service.
-	echo Manages print jobs sent from the computer to the printer or print server.
-	echo It can store multiple print jobs in the print queue or buffer retrieved by the printer or print server.
-	!toggleshow!
-	set svcnme=Spooler
-	!svcopt!
-	set svcnme=Fax
-	!svcopt!
-	cls
-
-	echo Name: Windows Image Acquisition
-	echo Waits until you press the button on your scanner and then manages the process of getting the image where it needs to go.
-	echo This also affects communication with cameras and Android PTP that you connect directly to your computer, so be aware of that if you need this function.
-	!toggleshow!
-	set svcnme=StiSvc
-	!svcopt!
-	cls
-
-	echo Name: Blutooth(BTAGService, bthserv)
-        echo Disable if you do not use bluetooth
-	echo BTAGService: Service supporting the audio gateway role of the Bluetooth Handsfree Profile.
-	echo bthserv: The Bluetooth service supports discovery and association of remote Bluetooth devices.
-	echo Stopping or disabling this service may cause paired Bluetooth devices to fail to operate properly and prevent new devices from being discovered or paired.
-	echo This can also serve as a safety measure from attacks. eg. KNOB, BLUFFS.
-	!toggleshow!
-	set svcnme=BTAGService
-	!svcopt!
-	set svcnme=bthserv
-	!svcopt!
-	cls
-
-	echo Name: Remote Access(SessionEnv, TermService, UmRdpService, RemoteRegistry)
-	echo These services make remote control of your computer possible.
-	echo If you don't use the remote desktop functionality of Windows, disable all these services.
-	echo However, Microsoft Support can potentially use remote desktop technology to diagnose issues you might be experiencing.
-	echo Windows's Remote support won't work if you disable these services.
-	echo This may also be a serious security issue and is used in fake support scams.
-	echo So, disabling these services can also help improve the security and provide some performance to your computer.
-	echo You may use Chrome Remote Desktop or most other 3rd party apps without any issues.
-	!toggleshow!
-	set svcnme=SessionEnv
-	!svcopt!
-	set svcnme=TermService
-	!svcopt!
-	set svcnme=UmRdpService
-	!svcopt!
-        set svcnme=RemoteRegistry
-	!svcopt!
-	cls
-
-	echo Name: System accuracy(HPET)
-	echo Type: Program
-	echo This command forces the kernel timer to constantly poll for interrupts instead of wait for them.
-	echo You should not change this
-	!toggleshow!
-	if !el!==1 (bcdedit /set useplatformclock true)
-	if !el!==2 (bcdedit /deletevalue useplatformclock)
-        cls
-
-        echo Please open your web browser and change your flags accordingly
-        %seperator%
-        For Chrome/Edge/Brave or other Chromium based browsers,
-	type chromium.txt
-	%seperator%
-	For Firefox/Zen/Tor or other Gecko based browsers,
-	type firefox.txt
-	%seperator%
-	echo Spam any key 5 times to exit...
-	pause>nul
-	pause>nul
-	pause>nul
-	pause>nul
-	pause>nul
-)
-
-net stop wuauserv>nul
-del /F /S /Q %windir%\SoftwareDistribution\Download\*>nul
-del /F /S /Q %tmp%\*>nul
-del /F /S /Q %windir%\Temp\*>nul
-del /F /S /Q %windir%\Prefetch\*>nul
-ipconfig /flushdns>nul
-ipconfig /registerdns>nul
-ipconfig /release>nul
-ipconfig /renew>nul
-wuauclt.exe /updatenow>nul
-net start wuauserv>nul
+::Special Tweak
+::powercfg.exe -import "!cd!\powerplan.pow">nul
 
 exit
 endlocal
+
 
 
 
