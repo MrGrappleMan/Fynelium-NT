bc3e9688a35392d7d5576f36e11f8091584fa3b7
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-09-27 01:46:48 +0530

    Update services.ps1

--- services.ps1
+++ services.ps1
@@ -1,54 +1,28 @@
-Start-Service -Name "SensrSvc"
-Set-Service -Name "SensrSvc" -StartupType Automatic
-Start-Service -Name "SensorService"
-Set-Service -Name "SensorService" -StartupType Automatic
-Start-Service -Name "NetTcpPortSharing"
-Set-Service -Name "NetTcpPortSharing" -StartupType Automatic
-Start-Service -Name "wisvc"
-Set-Service -Name "wisvc" -StartupType Automatic
-Start-Service -Name "WpnUserService"
-Set-Service -Name "WpnUserService" -StartupType Automatic
-Start-Service -Name "WpnService"
-Set-Service -Name "WpnService" -StartupType Automatic
-Start-Service -Name "UserDataSvc"
-Set-Service -Name "UserDataSvc" -StartupType Automatic
-Start-Service -Name "UnistoreSvc"
-Set-Service -Name "UnistoreSvc" -StartupType Automatic
-Start-Service -Name "UevAgentService"
-Set-Service -Name "UevAgentService" -StartupType Automatic
-Start-Service -Name "UsoSvc"
-Set-Service -Name "UsoSvc" -StartupType Automatic
-Start-Service -Name "InstallServicec"
-Set-Service -Name "InstallService" -StartupType Automatic
-Start-Service -Name "DiagTrack"
-Set-Service -Name "DiagTrack" -StartupType Automatic
-Start-Service -Name "tzautoupdate"
-Set-Service -Name "tzautoupdate" -StartupType Automatic
-Start-Service -Name "BITS"
-Set-Service -Name "BITS" -StartupType Automatic
-Start-Service -Name "DoSvc"
-Set-Service -Name "DoSvc" -StartupType Automatic
-Start-Service -Name "wuauserv"
-Set-Service -Name "wuauserv" -StartupType Automatic
-Start-Service -Name "WaaSMedicSvc"
-Set-Service -Name "WaaSMedicSvc" -StartupType Automatic
-Start-Service -Name "Dnscache"
-Set-Service -Name "Dnscache" -StartupType Automatic
-Start-Service -Name "svsvc"
-Set-Service -Name "svsvc" -StartupType Automatic
-Start-Service -Name "Winmgmt"
-Set-Service -Name "Winmgmt" -StartupType Automatic
-Start-Service -Name "whesvc"
-Set-Service -Name "whesvc" -StartupType Automatic
-Start-Service -Name "WebClient"
-Set-Service -Name "WebClient" -StartupType Automatic
-Start-Service -Name "W32Time"
-Set-Service -Name "W32Time" -StartupType Automatic
-Start-Service -Name "WlanSvc"
-Set-Service -Name "WlanSvc" -StartupType Automatic
-Start-Service -Name "dot3svc"
-Set-Service -Name "dot3svc" -StartupType Automatic
-Start-Service -Name "SysMain"
-Set-Service -Name "SysMain" -StartupType Automatic
-Start-Service -Name "WSearch"
-Set-Service -Name "WSearch" -StartupType Automatic
+# Check for admin privileges
+$isAdmin = ([System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    Write-Host "Re-run with admin rights" -ForegroundColor Red
+    Write-Host "Press Enter to exit"
+    Read-Host
+    exit
+}
+
+$services = @(
+    "SensrSvc", "SensorService", "NetTcpPortSharing", "wisvc", "WpnUserService",
+    "WpnService", "UserDataSvc", "UnistoreSvc", "UevAgentService", "UsoSvc",
+    "InstallService", "DiagTrack", "tzautoupdate", "BITS", "DoSvc", "wuauserv",
+    "WaaSMedicSvc", "Dnscache", "svsvc", "Winmgmt", "whesvc", "WebClient",
+    "W32Time", "WlanSvc", "dot3svc", "SysMain", "WSearch", "PrintNotify",
+    "Spooler", "Fax", "PrintWorkflowUserSvc", "StiSvc", "FrameServer", "WiaRpc"
+)
+
+foreach ($svcName in $services) {
+    $service = Get-Service -Name $svcName -ErrorAction SilentlyContinue
+    if ($service) {
+        Start-Service -Name $svcName -ErrorAction SilentlyContinue
+        Set-Service -Name $svcName -StartupType Automatic -ErrorAction SilentlyContinue
+        Write-Host "Started and set $svcName to Automatic" -ForegroundColor Green
+    } else {
+        Write-Host "Service $svcName not found." -ForegroundColor Yellow
+    }
+}
