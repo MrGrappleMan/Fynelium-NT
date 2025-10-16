d83ba370a5b5489c9c1a5481c6f8e70ac39e490a
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-17 01:41:48 +0530

    Enhance refresh.ps1 with comments for clarity
    Added comments for clarity on specific commands.

--- script/refresh.ps1
+++ script/refresh.ps1
@@ -1,21 +1,24 @@
+# Serves as a form of shotgun debugging without resetting your system
+
 sfc /scannow
 chkdsk /f /r
 wsreset.exe
 dism /Online /CheckHealth
 dism /Online /ScanHealth
 dism /Online /Cleanup-Image /RestoreHealth
-w32tm /resync /rediscover
+w32tm /resync /rediscover # NTP refresh
 ipconfig /release
 ipconfig /renew
 ipconfig /release6
 ipconfig /renew6
 ipconfig /flushdns
 ipconfig /registerdns
-netsh winsock reset
+netsh winsock reset # Should remove later
 netsh ip reset
-Start-MpWDOScan
-winget upgrade --all
+winget upgrade --all # Helps with updating system packages
 
 Remove-Item -Path $env:windir\Temp -Recurse -Force
 Remove-Item -Path $env:windir\Prefetch -Recurse -Force
 Remove-Item -Path $env:SystemDrive\Windows.old -Recurse -Force
+
+Start-MpWDOScan # Restarts the device for Defender offline scan
