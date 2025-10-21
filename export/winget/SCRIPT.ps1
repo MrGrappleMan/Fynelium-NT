cd31ec510bc1c9351cdc019440a1ec0859c1dabd
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-21 15:20:19 +0530

    Delete export/winget/SCRIPT.ps1

--- export/winget/SCRIPT.ps1
+++
@@ -1,3 +0,0 @@
-winget upgrade --all # Update
-Set-Location "$Env:windir\\Temp\\Fynelium-NT\\export\\winget\\"
-winget import -i .json --ignore-unavailable --ignore-versions --accept-package-agreements --accept-source-agreements
