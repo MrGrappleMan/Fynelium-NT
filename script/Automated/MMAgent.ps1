51581823bcbec9621c49b5a9a69a536e27a045a9
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-24 01:39:58 +0530

    Create MMAgent.ps1

---
+++ script/Automated/MMAgent.ps1
@@ -0,0 +1,7 @@
+# 📒 Memory Management Agent ( MMAgent )
+Enable-MMAgent -ApplicationLaunchPrefetching
+Enable-MMAgent -ApplicationPreLaunch
+Enable-MMAgent -MemoryCompression # Like ZRAM
+Enable-MMAgent -OperationAPI
+Enable-MMAgent -PageCombining
+Set-MMAgent -MaxOperationAPIFiles 8192
