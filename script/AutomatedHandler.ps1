e354a3fd76ba50a89c926e6a8faebf7f3f4f531d
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-24 01:52:29 +0530

    Create AutomatedHandler.ps1

---
+++ script/AutomatedHandler.ps1
@@ -0,0 +1,2 @@
+# 🔱 Automated Handler - Executes each script in the Automated folder for automatically performing the tweaking process
+# Scripts that require zero user interaction, or can be forced to do so, go in here
