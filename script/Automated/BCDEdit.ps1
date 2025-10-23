f0ea59f81bf78bdd0609bc72c9e46fd0247feada
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-24 01:40:33 +0530

    Create BCDEdit.ps1

---
+++ script/Automated/BCDEdit.ps1
@@ -0,0 +1,27 @@
+# 🌱 Boot Configuration Data Editor ( BCDEdit )
+bcdedit /set bootlog no # Only for debugging
+bcdedit /set bootmenupolicy Standard
+bcdedit /set bootstatuspolicy DisplayAllFailures
+bcdedit /set quietboot on 
+bcdedit /set sos off
+#bcdedit /set nocrashautoreboot off
+bcdedit /set bootuxdisabled off # Keeps windows boot experience
+#bcdedit /set maxproc yes
+bcdedit /set disabledynamictick no
+#bcdedit /set usefirmwarepcisettings no
+bcdedit /set nointegritychecks off
+#bcdedit /set groupaware on
+#bcdedit /set maxgroup on
+#bcdedit /set onecpu off
+#bcdedit /set vsmlaunchtype Auto
+bcdedit /set nx Optin
+#bcdedit /deletevalue useplatformclock
+#bcdedit /set forcelegacyplatform no
+#bcdedit /set nolowmem off
+#bcdedit /set x2apicpolicy enable
+#bcdedit /set useplatformtick yes
+#bcdedit /set tscsyncpolicy enhanced
+#bcdedit /set uselegacyapicmode no
+#bcdedit /set usephysicaldestination no
+#bcdedit /set tpmbootentropy default
+#bcdedit /set testsigning No
