fd1253d97ca562eb9a7d0ff817a57efc9b79751f
Avaneesh Jambhale <90239357+MrGrappleMan@users.noreply.github.com>
2025-10-24 10:06:00 +0530

    Update PowerCfg.ps1

--- script/Automated/PowerCfg.ps1
+++ script/Automated/PowerCfg.ps1
@@ -4,8 +4,8 @@ powercfg /Change monitor-timeout-ac 3 # Turn screen off after 3 min while chargi
 powercfg /Change monitor-timeout-dc 3 # Turn screen off after 3 min on battery
 powercfg /Change disk-timeout-ac 1 # Turns drive off after 1 min while charging. If the device is a NAS or a file or disk recovery station dealing w/ HDDs, set this to 0.
 powercfg /Change disk-timeout-dc 1 # Turns drive off after 1 min while on battery. Assuming you use SSDs, this will save you A LOT of energy, despite SSDs aready being efficient.
-powercfg /Change standby-timeout-ac 0 # Never sleep while charging
+powercfg /Change standby-timeout-ac 30 # Sleep after 30 min while charging
 powercfg /Change standby-timeout-dc 5 # Sleep after 5 min on battery
-powercfg /Change hibernate-timeout-ac 0 # Never hibernate while charging
+powercfg /Change hibernate-timeout-ac 60 # Hibernate after 60 min while charging
 powercfg /Change hibernate-timeout-dc 15 # Hibernate after 15 min on battery
 ##powercfg.exe -import "!cd!\powerplan.pow">nul
